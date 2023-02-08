41b7c346455cf32d0681c33819e5619c4c21263e
xdadev37 <pouriasa37@gmail.com>
2023-02-08 15:49:07 +0330

    cors fixed

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = withWorkbox({
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['localhost', 'cms.consolex.ir'],
+    domains: ['localhost', 'consolex.ir'],
   },
   workbox: {
     swSrc: './sw.ts',
