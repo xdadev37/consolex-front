7089ea87becc60ea95db4c9854d90cfff8e63d41
PouRia SA <pouriasa.work@outlook.com>
2023-02-11 22:27:44 +0330

    sw optimized

--- sw.ts
+++ sw.ts
@@ -1,4 +1,5 @@
 import 'workbox-precaching'
+import { enable as preNavEnable } from 'workbox-navigation-preload'
 import { setDefaultHandler } from 'workbox-routing'
 import { clientsClaim } from 'workbox-core'
 import { NetworkOnly } from 'workbox-strategies'
@@ -6,5 +7,6 @@ import { NetworkOnly } from 'workbox-strategies'
 declare const self: ServiceWorkerGlobalScope
 
 const ignored = self.__WB_MANIFEST
-setDefaultHandler(new NetworkOnly())
+preNavEnable()
+setDefaultHandler(new NetworkOnly({ networkTimeoutSeconds: 30000 }))
 clientsClaim()
