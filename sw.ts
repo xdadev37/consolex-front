e9acf253ea454a00a8a557d13ba768f1e78bc39a
PouRia SA <pouriasa.work@outlook.com>
2023-02-26 01:59:28 +0330

    sw strategy optimized

--- sw.ts
+++ sw.ts
@@ -1,12 +1,36 @@
 import 'workbox-precaching'
+import axios from 'axios'
 import { enable as preNavEnable } from 'workbox-navigation-preload'
 import { setDefaultHandler } from 'workbox-routing'
 import { clientsClaim } from 'workbox-core'
-import { StaleWhileRevalidate } from 'workbox-strategies'
+import { CacheFirst } from 'workbox-strategies'
 
 declare const self: ServiceWorkerGlobalScope
 
 const ignored = self.__WB_MANIFEST
 preNavEnable()
-setDefaultHandler(new StaleWhileRevalidate())
+setDefaultHandler(
+  new CacheFirst({
+    plugins: [
+      {
+        handlerWillStart: async ({ request }) =>
+          request.destination === 'image'
+            ? caches
+                .open('cache')
+                .then(c =>
+                  c
+                    .match(request.url)
+                    .then(
+                      r =>
+                        r ||
+                        axios({ url: request.url }).then(res =>
+                          c.put(request, res.data).then(() => res.data)
+                        )
+                    )
+                )
+            : undefined,
+      },
+    ],
+  })
+)
 clientsClaim()
