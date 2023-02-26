8ae1e6c888a46bb886ce497e7645d4a4c50b569e
xdadev37 <pouriasa37@gmail.com>
2023-02-26 14:42:00 +0330

    sw caching added

--- AppSettings.ts
+++ AppSettings.ts
@@ -1,6 +1,6 @@
 enum appSettings {
-  baseUrl = 'http://localhost:1337',
-  // baseUrl = 'https://consolex.ir',
+  // baseUrl = 'http://localhost:1337',
+  baseUrl = 'https://consolex.ir',
 }
 
 export default appSettings
--- sw.ts
+++ sw.ts
@@ -1,5 +1,6 @@
 import 'workbox-precaching'
 import axios from 'axios'
+import appSettings from 'AppSettings'
 import { enable as preNavEnable } from 'workbox-navigation-preload'
 import { setDefaultHandler } from 'workbox-routing'
 import { clientsClaim } from 'workbox-core'
@@ -13,22 +14,23 @@ setDefaultHandler(
   new CacheFirst({
     plugins: [
       {
-        handlerWillStart: async ({ request }) =>
+        handlerWillRespond: async ({ request }) =>
           request.destination === 'image'
-            ? caches
-                .open('cache')
-                .then(c =>
-                  c
-                    .match(request.url)
-                    .then(
-                      r =>
-                        r ||
-                        axios({ url: request.url }).then(res =>
-                          c.put(request, res.data).then(() => res.data)
-                        )
-                    )
+            ? caches.open('cache').then(c =>
+                c.match(request.url).then(
+                  r =>
+                    r ||
+                    axios({
+                      baseURL: appSettings.baseUrl,
+                      url: request.url,
+                      responseType: 'blob',
+                    }).then(res => res.data)
                 )
-            : undefined,
+              )
+            : axios({
+                baseURL: appSettings.baseUrl,
+                url: request.url,
+              }).then(res => res.data),
       },
     ],
   })
