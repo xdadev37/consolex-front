aea658af44d3b9b20d93b956bcb7d6a7b24ae81d
xdadev37 <pouriasa37@gmail.com>
2023-10-10 20:33:10 +0000

    seo description added

--- Constants/constants.enum.ts
+++ Constants/constants.enum.ts
@@ -1,5 +1,5 @@
 enum constants {
-  description = 'استعلام قیمت خرید ps5 ps4 xbox پلی استیشن ایکس باکس اصفهان خرید دسته کنسول بازی اصفهان',
+  description = 'استعلام قیمت خرید ps5 ps4 xbox پلی استیشن ایکس باکس اصفهان خرید دسته کنسول نصب بازی اصفهان',
 }
 
 export default constants
