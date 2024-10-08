dffc5851bcbcb7d5846186acaffe5e95a5bbaba8
PouRia SA <pouriasa37@gmail.com>
2024-10-08 10:41:10 +0000

    sw fixed

--- sw.ts
+++ sw.ts
@@ -1,17 +1,18 @@
 import 'workbox-precaching'
 import { enable as preNavEnable } from 'workbox-navigation-preload'
-import { registerRoute, Route } from 'workbox-routing'
+import { registerRoute, NavigationRoute } from 'workbox-routing'
 import { clientsClaim } from 'workbox-core'
-import { NetworkFirst } from 'workbox-strategies'
+import { CacheFirst, NetworkOnly } from 'workbox-strategies'
 
 declare const self: ServiceWorkerGlobalScope
 
 const ignored = self.__WB_MANIFEST
 
 preNavEnable()
-const imageRoute = new Route(
-  ({ request }) => request.destination === 'image',
-  new NetworkFirst()
+const imageRoute = new NavigationRoute(props =>
+  props.request.destination === 'image'
+    ? new CacheFirst().handle(props)
+    : new NetworkOnly().handle(props)
 )
 registerRoute(imageRoute)
 clientsClaim()
